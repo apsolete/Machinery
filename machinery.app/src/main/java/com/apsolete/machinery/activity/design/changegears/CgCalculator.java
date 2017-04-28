b9153fe652506078a48ee836b3364bfc70959ae7
qelveen <qelveen@gmail.com>
2017-04-29 01:34:49 +0300

    Improvement CgCalc

--- machinery.app/src/main/java/com/apsolete/machinery/activity/design/changegears/CgCalculator.java
+++ machinery.app/src/main/java/com/apsolete/machinery/activity/design/changegears/CgCalculator.java
@@ -42,21 +42,16 @@ public class CgCalculator extends AsyncTask<int[], Integer, Void>
         {
             if (gs3 == null || gs4 == null)
             {
-                //_pb.setMax(z1Gears.length * z2Gears.length);
-                //clear();
-                //int p = 1;
                 calculateBy(gs1, gs2);
             }
             else if (gs3.length > 0 && gs4.length > 0)
             {
                 if (gs5 == null || gs6 == null)
                 {
-                    //clear();
                     calculateBy(gs1, gs2, gs3, gs4);
                 }
                 else if (gs5.length > 0 && gs6.length > 0)
                 {
-                    //clear();
                     calculateBy(gs1, gs2, gs3, gs4, gs5, gs6);
                 }
             }
@@ -74,19 +69,25 @@ public class CgCalculator extends AsyncTask<int[], Integer, Void>
     private void calculateBy(int[] gs1, int[] gs2)
     {
         // calculate by z1, z2
+        int count = gs1.length * gs2.length;
+        int i = 0;
         for (int z1: gs1)
         {
+            i++;
             for (int z2: gs2)
             {
+                i++;
                 if (_diffTeethGearing && z1 == z2)
                     continue;
 
-                //_pb.setProgress(p++);
                 double ratio = (double)z1 / (double)z2;
                 if (checkRatio(ratio))
                 {
-                    if (!setResult(ratio, z1, z2, 0, 0, 0, 0))
-                        break;
+                    if (_resultListener != null)
+                    {
+                        publishProgress((100 * i)/count);
+                        _resultListener.onResult(ratio, new int[]{z1, z2, 0, 0, 0, 0});
+                    }
                 }
             }
         }
@@ -95,28 +96,37 @@ public class CgCalculator extends AsyncTask<int[], Integer, Void>
     private void calculateBy(int[] gs1, int[] gs2, int[] gs3, int[] gs4)
     {
         // calculate by z1, z2, z3, z4
+        int count = gs1.length * gs2.length * gs3.length * gs4.length;
+        int i = 0;
         for (int z1: gs1)
         {
+            i++;
             for (int z2: gs2)
             {
+                i++;
                 if (_diffTeethGearing && z1 == z2)
                     continue;
 
                 for (int z3: gs3)
                 {
+                    i++;
                     if (_diffTeethDoubleGear && z2 == z3)
                         continue;
 
                     for (int z4: gs4)
                     {
+                        i++;
                         if (_diffTeethGearing && z3 == z4)
                             continue;
 
                         double ratio = (double)(z1 * z3) / (double)(z2 * z4);
                         if (checkRatio(ratio))
                         {
-                            if (!setResult(ratio, z1, z2, z3, z4, 0, 0))
-                                break;
+                            if (_resultListener != null)
+                            {
+                                publishProgress((100 * i)/count);
+                                _resultListener.onResult(ratio, new int[]{z1, z2, z3, z4, 0, 0});
+                            }
                         }
                     }
                 }
@@ -127,38 +137,49 @@ public class CgCalculator extends AsyncTask<int[], Integer, Void>
     private void calculateBy(int[] gs1, int[] gs2, int[] gs3, int[] gs4, int[] gs5, int[] gs6)
     {
         // calculate by z1, z2, z3, z4, z6
+        int count = gs1.length * gs2.length * gs3.length * gs4.length * gs5.length * gs6.length;
+        int i = 0;
         for (int z1: gs1)
         {
+            i++;
             for (int z2: gs2)
             {
+                i++;
                 if (_diffTeethGearing && z1 == z2)
                     continue;
 
                 for (int z3: gs3)
                 {
+                    i++;
                     if (_diffTeethDoubleGear && z2 == z3)
                         continue;
 
                     for (int z4: gs4)
                     {
+                        i++;
                         if (_diffTeethGearing && z1 == z2)
                             continue;
 
                         for (int z5: gs5)
                         {
+                            i++;
                             if (_diffTeethDoubleGear && z4 == z5)
                                 continue;
 
                             for (int z6: gs6)
                             {
+                                i++;
                                 if (_diffTeethGearing && z1 == z2)
                                     continue;
-
+                                    
                                 double ratio = (double)(z1 * z3 * z5) / (double)(z2 * z4 * z6);
                                 if (checkRatio(ratio))
                                 {
-                                    if (!setResult(ratio, z1, z2, z3, z4, z5, z6))
-                                        break;
+                                    if (_resultListener != null)
+                                    {
+                                        publishProgress((100 * i) / count);
+                                        _resultListener.onResult(ratio, new int[]{z1, z2, z3, z4, z5, z6});
+                                    }
                                 }
                             }
                         }
@@ -168,13 +189,12 @@ public class CgCalculator extends AsyncTask<int[], Integer, Void>
         }
     }
 
-    private boolean setResult(double ratio, int z1, int z2, int z3, int z4, int z5, int z6)
+    private void setResult(double ratio, int z1, int z2, int z3, int z4, int z5, int z6)
     {
         if (_resultListener == null)
-            return false;
+            return;
             
         _resultListener.onResult(ratio, new int[]{z1, z2, z3, z4, z5, z6});
-        return true;
     }
     
     private boolean checkRatio(double ratio)
