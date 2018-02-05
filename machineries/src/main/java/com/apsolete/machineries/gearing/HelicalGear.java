09b8eaa5012f2868e2b37c0d139dcbbdb79aa0b0
qelveen <qelveen@gmail.com>
2018-02-06 00:01:07 +0300

    Added new module machineries

---
+++ machineries/src/main/java/com/apsolete/machineries/gearing/HelicalGear.java
@@ -0,0 +1,9 @@
+package com.apsolete.machinery.gearing;
+
+public class HelicalGear extends Gear
+{
+    public HelicalGear(int teeth, double module, double tiltAngle)
+    {
+        super(teeth, module, tiltAngle);
+    }
+}
