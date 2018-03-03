6292328808decd8fd0f09f52f45087d0fbb40a5b
qelveen <qelveen@gmail.com>
2018-03-04 00:29:21 +0300

    Remove unused files

--- machinery.gearing/src/main/java/com/apsolete/machinery/gearing/HelicalGear.java
+++
@@ -1,9 +0,0 @@
-package com.apsolete.machinery.gearing;
-
-public class HelicalGear extends Gear
-{
-    public HelicalGear(int teeth, double module, double tiltAngle)
-    {
-        super(teeth, module, tiltAngle);
-    }
-}
--- machinery.gearing/src/main/java/com/apsolete/machinery/gearing/SpurGear.java
+++
@@ -1,9 +0,0 @@
-package com.apsolete.machinery.gearing;
-
-public class SpurGear extends Gear
-{
-    public SpurGear(int teeth, double module)
-    {
-        super(teeth, module, 0.0);
-    }
-}
