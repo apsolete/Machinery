1a5446a0a551422bd95948fb64487695476b45bc
Qelveen <qelveen@gmail.com>
2019-03-10 01:13:44 +0300

    Added models

---
+++ app/src/main/java/com/apsolete/machinery/calculation/belting/pbelt/Model.java
@@ -0,0 +1,52 @@
+package com.apsolete.machinery.calculation.belting.pbelt;
+
+import com.apsolete.machinery.calculation.CalculationModel;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Model extends CalculationModel
+{
+	public Model()
+	{
+
+	}
+
+	public Model(Parcel in)
+	{
+
+	}
+
+	@Override
+	protected void calculateInternal()
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public int describeContents()
+	{
+		// TODO: Implement this method
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel out, int flags)
+	{
+		// TODO: Implement this method
+	}
+
+	public static final Parcelable.Creator<Model> CREATOR = new Parcelable.Creator<Model>()
+    {
+        @Override
+        public Model createFromParcel(Parcel parcel)
+        {
+            return new Model(parcel);
+        }
+
+        @Override
+        public Model[] newArray(int size)
+        {
+            return new Model[size];
+        }
+    };
+}
